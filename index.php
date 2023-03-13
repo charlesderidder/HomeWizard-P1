fe70bfdcc1250fd25f7ebf30364728630113b896
charlesderidder <51724441+charlesderidder@users.noreply.github.com>
2023-03-13 11:28:52 +0100

    Update index.php
    Negative values are now displayed in green.
    High rate is shown in red
    Low rate is shown in green

--- index.php
+++ index.php
@@ -1,28 +1,90 @@
 <?php
+
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "http://192.168.178.13/api/v1/data"); // wijzig ipadres in het ipadres van je HomeWizard P1 meter
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $result = curl_exec($ch);
+
 if (curl_errno($ch)) { echo curl_error($ch); }
 else {
- // echo $result;
+ // echo $result; / STRING  verwijder "//" om alle data weer te geven
   $decoded = json_decode($result);
-echo "<b> METER INFO </b> <br/><b> SMR Versie: </b>$decoded->smr_version<br/>";
-echo "<b> Meter Model: </b>$decoded->meter_model<br/>";
-echo "<b> WiFi SSID: </b> $decoded->wifi_ssid<br/>";
-echo "<b> WiFi signaalsterkte: </b>$decoded->wifi_strength<br/><br/>";
+  $active_power_w = $decoded->active_power_w;
+  $active_power_l1_w = $decoded->active_power_l1_w;
+  $active_power_l2_w = $decoded->active_power_l2_w;
+  $active_power_l3_w = $decoded->active_power_l3_w;
+  $meter_model = $decoded->meter_model;
+  
+  if ($active_power_w < 0) {
+    echo "<b>Verbruik: </b><span style=\"color:green;\">$active_power_w Watt</span><br/>";
+  } else {
+    echo "<b>Verbruik: </b>$active_power_w Watt<br/>";
+  }
 
-echo "<b>STROOM IMPORT</b><br/><b>Totaal Import T1: </b>$decoded->total_power_import_t1_kwh kWh <br/>";
-echo "<b> Totaal Import T2: </b>$decoded->total_power_import_t2_kwh kWh <br/><br/>";
 
-echo "<b>STROOM EXPORT</b><br/><b> Totaal Export T1: </b>$decoded->total_power_export_t1_kwh kWh <br/>";
-echo "<b> Totaal Export T2: </b>$decoded->total_power_export_t2_kwh kWh <br/><br/>";
+//echo "<b>Actief Tarief: </b>$decoded->active_tariff<br/></b>";
+  if ($active_tariff = 2) {
+    echo "<td>Huidig tarief: <span style=\"color:red;\">Hoog</span></td><br>";
+  } else {
+    echo "<td>Huidig tarief: <span style=\"color:green;\">Laag</span></td><br>";
+  }
 
-echo "<b>HUIDIG VERBRUIK</b><br/><b> Totaal stroomverbruik:</b> $decoded->active_power_w Watt <br/>";
-echo "<b>Totaal L1: </b>$decoded->active_power_l1_w Watt <br/>";
-echo "<b>Totaal L2: </b>$decoded->active_power_l2_w Watt <br/>";
-echo "<b> Totaal L3: </b>$decoded->active_power_l3_w Watt<br/><br/>";
 
+echo "<b>Meter Model: </b>$decoded->meter_model<br/></b>";
+
+// Display import/export/total data in table
+echo "<table border='1'><tr><th></th><th> Import </th><th> Export</th></tr>";
+echo "<tr> <td>T1</td>";
+echo "<td> <b style='color:red;'>$decoded->total_power_import_t1_kwh</b></td>";
+echo "<td> <b style='color:green;'>$decoded->total_power_export_t1_kwh</b></td>";
+echo "</tr>";
+echo "<tr><td>T2</td>";
+echo "<td> <b style='color:red;'>$decoded->total_power_import_t2_kwh</b></td>";
+echo "<td> <b style='color:green;'>$decoded->total_power_export_t2_kwh</b></td>";
+echo "</tr>";
+echo "<tr><td>Totaal</td>";
+echo "<td> <b style='color:red;'>$decoded->total_power_import_kwh</b></td>";
+echo "<td> <b style='color:green;'>$decoded->total_power_export_kwh</b></td>";
+echo "</tr>";	 
+echo "</table>";
+
+// Display Fase/Watt/Voltage/Ampere in a table
+echo "<table border='1'><tr><th>Fase </th><th> Watt </th><th> Volt</th><th> Ampere</th></tr>";
+echo "<tr> <td>L1</td>";
+
+  if ($active_power_l1_w < 0) {
+    echo "<td><span style=\"color:green;\">$active_power_l1_w</span></td>";
+  } else {
+    echo "<td>$active_power_l1_w</td>";
+  }
+
+echo "<td> $decoded->active_voltage_l1_v</td>";
+echo "<td> $decoded->active_current_l1_a</td>";
+echo "</tr>";
+echo "<tr><td>L2</td>";
+  if ($active_power_l2_w < 0) {
+    echo "<td><span style=\"color:green;\">$active_power_l2_w</span></td>";
+  } else {
+    echo "<td>$active_power_l2_w</td>";
+  }
+
+echo "<td> $decoded->active_voltage_l2_v</td>";
+echo "<td> $decoded->active_current_l2_a</td>";
+echo "</tr>";	
+echo "<tr><td>L3</td>";
+  if ($active_power_l3_w < 0) {
+    echo "<td><span style=\"color:green;\">$active_power_l3_w</span></td>";
+  } else {
+    echo "<td>$active_power_l3_w</td>";
+  }
+echo "<td> $decoded->active_voltage_l3_v</td>";
+echo "<td> $decoded->active_current_l3_a</td>";
+echo "</tr>";   
+echo "</table>";
+
+echo "<b>Stroomuitval: </b>$decoded->any_power_fail_count	<br/>";
+echo "<b>Stroomuitval lang: </b>$decoded->long_power_fail_count	<br/>";
 }
-curl_close($ch);
+
+
 ?>
