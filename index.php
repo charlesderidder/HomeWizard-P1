3edffe5deb71e1244593a189e1eb92c0ade66769
charlesderidder <51724441+charlesderidder@users.noreply.github.com>
2023-03-15 07:41:01 +0100

    Update index.php
    fixed tariff high/low

--- index.php
+++ index.php
@@ -13,7 +13,11 @@ else {
   $active_power_l1_w = $decoded->active_power_l1_w;
   $active_power_l2_w = $decoded->active_power_l2_w;
   $active_power_l3_w = $decoded->active_power_l3_w;
+  $active_voltage_l1_v = $decoded->active_voltage_l1_v;
+  $active_voltage_l2_v = $decoded->active_voltage_l2_v;
+  $active_voltage_l3_v = $decoded->active_voltage_l3_v;
   $meter_model = $decoded->meter_model;
+  $active_tariff = $decoded->active_tariff;
   
   if ($active_power_w < 0) {
     echo "<b>Verbruik: </b><span style=\"color:green;\">$active_power_w Watt</span><br/>";
@@ -21,14 +25,11 @@ else {
     echo "<b>Verbruik: </b>$active_power_w Watt<br/>";
   }
 
-
-//echo "<b>Actief Tarief: </b>$decoded->active_tariff<br/></b>";
-  if ($active_tariff = 2) {
+  if ($active_tariff > 1) {
     echo "<td>Huidig tarief: <span style=\"color:red;\">Hoog</span></td><br>";
   } else {
     echo "<td>Huidig tarief: <span style=\"color:green;\">Laag</span></td><br>";
   }
-
 
 echo "<b>Meter Model: </b>$decoded->meter_model<br/></b>";
 
@@ -56,9 +57,14 @@ echo "<tr> <td>L1</td>";
     echo "<td><span style=\"color:green;\">$active_power_l1_w</span></td>";
   } else {
     echo "<td>$active_power_l1_w</td>";
+  }  
+  
+  if ($active_voltage_l1_v > 240) {
+    echo "<td><span style=\"color:red;\">$active_voltage_l1_v</span></td>";
+  } else {
+    echo "<td>$active_voltage_l1_v</td>";
   }
 
-echo "<td> $decoded->active_voltage_l1_v</td>";
 echo "<td> $decoded->active_current_l1_a</td>";
 echo "</tr>";
 echo "<tr><td>L2</td>";
@@ -68,7 +74,12 @@ echo "<tr><td>L2</td>";
     echo "<td>$active_power_l2_w</td>";
   }
 
-echo "<td> $decoded->active_voltage_l2_v</td>";
+  if ($active_voltage_l2_v > 240) {
+    echo "<td><span style=\"color:red;\">$active_voltage_l2_v</span></td>";
+  } else {
+    echo "<td>$active_voltage_l2_v</td>";
+  }
+  
 echo "<td> $decoded->active_current_l2_a</td>";
 echo "</tr>";	
 echo "<tr><td>L3</td>";
@@ -77,7 +88,11 @@ echo "<tr><td>L3</td>";
   } else {
     echo "<td>$active_power_l3_w</td>";
   }
-echo "<td> $decoded->active_voltage_l3_v</td>";
+  if ($active_voltage_l3_v > 240) {
+    echo "<td><span style=\"color:red;\">$active_voltage_l3_v</span></td>";
+  } else {
+    echo "<td>$active_voltage_l3_v</td>";
+  }
 echo "<td> $decoded->active_current_l3_a</td>";
 echo "</tr>";   
 echo "</table>";
@@ -85,6 +100,4 @@ echo "</table>";
 echo "<b>Stroomuitval: </b>$decoded->any_power_fail_count	<br/>";
 echo "<b>Stroomuitval lang: </b>$decoded->long_power_fail_count	<br/>";
 }
-
-
 ?>
